cd56c23026294ec4c147d59807c831d691c48d40
topdev17 <dannyjin117@gmail.com>
2021-12-06 21:45:33 -0500

    add new features

--- components/Connect4.js
+++ components/Connect4.js
@@ -1,6 +1,6 @@
-import { useReducer } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import { Row } from './Row'
-import { Button, Text } from '@chakra-ui/react'
+import { Button, Text, Flex, Box } from '@chakra-ui/react'
 import { checkForWin, deepCloneBoard, generateNewBoard } from '../gameUtils'
 import * as gameStyles from '../styles/Home.module.css'
 
@@ -10,12 +10,16 @@ const gameReducer = (state, action) => {
       return {
         ...initialGameState,
         board: action.board,
+        player1: action.starter === 'RED' ? 1 : 2,
+        player2: action.starter === 'RED' ? 2 : 1,
+        currentPlayer: action.starter === 'RED' ? 1 : 2,
       }
     case 'togglePlayer':
       return {
         ...state,
         currentPlayer: action.nextPlayer,
         board: action.board,
+        inProgress: true,
       }
     case 'endGame':
       return {
@@ -23,11 +27,20 @@ const gameReducer = (state, action) => {
         gameOver: true,
         message: action.message,
         board: action.board,
+        inProgress: false,
       }
     case 'updateMessage':
       return {
         ...state,
         message: action.message,
+      }
+    case 'nextRound':
+      return {
+        ...state,
+        message: '',
+        board: action.board,
+        gameOver: false,
+        currentPlayer: action.nextPlayer,
       }
     default:
       throw Error(`Action "${action.type}" is not a valid action.`)
@@ -47,8 +60,11 @@ const initialGameState = {
   ],
   gameOver: false,
   message: '',
+  inProgress: false,
 }
 export const Connect4 = () => {
+  const [starter, setStarter] = useState('RED')
+
   const [gameState, dispatchGameState] = useReducer(
     gameReducer,
     initialGameState
@@ -71,13 +87,13 @@ export const Connect4 = () => {
       if (result === gameState.player1) {
         dispatchGameState({
           type: 'endGame',
-          message: 'Player1 (red) wins!',
+          message: 'Player1 wins!',
           board,
         })
       } else if (result === gameState.player2) {
         dispatchGameState({
           type: 'endGame',
-          message: 'Player2 (yellow) wins!',
+          message: 'Player2 wins!',
           board,
         })
       } else if (result === 'draw') {
@@ -97,24 +113,80 @@ export const Connect4 = () => {
     }
     // it's gameover and a user clicked a cell
     else {
+      const nextPlayer =
+        gameState.currentPlayer === gameState.player1
+          ? gameState.player2
+          : gameState.player1
       dispatchGameState({
-        type: 'updateMessage',
-        message: 'Game Over. Please start a new game.',
+        type: 'nextRound',
+        board: generateNewBoard(),
+        nextPlayer,
       })
     }
   }
 
+  useEffect(() => {
+    if (!gameState.inProgress) {
+      dispatchGameState({ type: 'newGame', board: generateNewBoard(), starter })
+    }
+  }, [starter])
+
   return (
     <>
-      <Button
-        colorScheme="purple"
-        className={gameStyles.button}
-        onClick={() => {
-          dispatchGameState({ type: 'newGame', board: generateNewBoard() })
-        }}
-      >
-        New Game
-      </Button>
+      <Flex alignItems="center">
+        <Text fontSize="xl" fontWeight="bold" color="red.500">
+          Choose Who Goes First:
+        </Text>
+        <Box
+          className={gameStyles.player1}
+          borderColor="blue.500"
+          borderWidth={starter === 'RED' ? 3 : 0}
+          onClick={() => setStarter('RED')}
+        />
+        <Box
+          className={gameStyles.player2}
+          borderColor="blue.500"
+          borderWidth={starter === 'YELLOW' ? 3 : 0}
+          onClick={() => setStarter('YELLOW')}
+        />
+      </Flex>
+      <Flex gridGap="20px">
+        <Button
+          colorScheme="purple"
+          className={gameStyles.button}
+          onClick={() => {
+            dispatchGameState({
+              type: 'newGame',
+              board: generateNewBoard(),
+              starter,
+            })
+          }}
+        >
+          New Game
+        </Button>
+
+        <Button
+          colorScheme="teal"
+          className={gameStyles.button}
+          onClick={() => {
+            const nextPlayer =
+              gameState.currentPlayer === gameState.player1
+                ? gameState.player2
+                : gameState.player1
+            dispatchGameState({
+              type: 'nextRound',
+              board: generateNewBoard(),
+              nextPlayer,
+            })
+          }}
+        >
+          Next Round
+        </Button>
+      </Flex>
+
+      <Text fontSize="lg" fontWeight="bold">
+        {gameState.message}
+      </Text>
 
       <table>
         <tbody>
@@ -123,8 +195,6 @@ export const Connect4 = () => {
           ))}
         </tbody>
       </table>
-
-      <Text>{gameState.message}</Text>
     </>
   )
 }
